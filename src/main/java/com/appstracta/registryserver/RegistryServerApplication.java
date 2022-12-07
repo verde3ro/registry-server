5a8f7e543c9290929b7d3e38c5e870a5c46de330
Raúl Verde <verde3ro@gmail.com>
2022-12-07 15:13:05 -0600

    Se agrega configuración de los 3 prieros endpoint

---
+++ src/main/java/com/appstracta/registryserver/RegistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.appstracta.registryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServerApplication.class, args);
+	}
+
+}
